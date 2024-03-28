15096df298f5ecee9a1c359c0944ecac4f8a69a1
Ignacio Viggiani <ign.v@hotmail.com>
2024-03-28 13:26:00 -0300

    Working initial main

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\ignv\Documents\Code\rust trials\rust_learn\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\ignv\Documents\Code\rust trials\rust_learn\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\ignv\Documents\Code\rust\ trials\rust_learn\guessing_game\target\debug\guessing_game.exe: C:\Users\ignv\Documents\Code\rust\ trials\rust_learn\guessing_game\src\main.rs
